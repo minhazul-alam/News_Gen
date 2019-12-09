d757bbdc05f5a235446d9103716dfe5f2b9a6324
Mohammad Minhazul Alam <minhaz.ulalam@gmail.com>
2019-12-09 23:03:02 +0600

    Update bengali_train_updated.py

--- Minhaz/Code/Bengali_Work/bengali_train_updated.py
+++ Minhaz/Code/Bengali_Work/bengali_train_updated.py
@@ -10,11 +10,13 @@ from keras.layers import Dense, Dropout, Activation, LSTM, Bidirectional
 import numpy as np
 import io
 import os
+from nltk.corpus.reader import PlaintextCorpusReader
+from nltk import RegexpTokenizer
 
 SEQUENCE_LEN = 8        #have to try different sequence length
 MIN_WORD_FREQUENCY = 10 #haven't used it here
 STEP = 1                #have to try 2/3
-BATCH_SIZE = 64         #can take larger
+BATCH_SIZE = 32         #can take larger
 
 def print_vocabulary(words_file_path, words_set):
     words_file = io.open(words_file_path, 'w', encoding='utf8')
@@ -56,23 +58,38 @@ def sample(preds, temperature=1.0):     #high temp ~ pick any, low temp ~ pick b
     probas = np.random.multinomial(1, preds, 1) #returns picked value
     return np.argmax(probas)    #returns index
 
+def shuffle_and_split_training_set(sentences_original, next_original, percentage_test=2):
+    # shuffle at unison
+    print('Shuffling sentences')
+
+    tmp_sentences = []
+    tmp_next_word = []
+    for i in np.random.permutation(len(sentences_original)):
+        tmp_sentences.append(sentences_original[i])
+        tmp_next_word.append(next_original[i])
+
+    cut_index = int(len(sentences_original) * (1.-(percentage_test/100.)))
+    x_train, x_test = tmp_sentences[:cut_index], tmp_sentences[cut_index:]
+    y_train, y_test = tmp_next_word[:cut_index], tmp_next_word[cut_index:]
+
+    print("Size of training set = %d" % len(x_train))
+    print("Size of test set = %d" % len(y_test))
+    return (x_train, y_train), (x_test, y_test)
+
 def on_epoch_end(epoch, logs):
     # Function invoked at end of each epoch. Prints generated text.
     examples_file.write('\n----- Generating text after Epoch: %d\n' % epoch)
-    if epoch % 2 == 1:
-        return
     # Randomly pick a seed sequence
-    seed_index = np.random.randint(len(sentences))
-    seed = (sentences)[seed_index]
+    seed_index = np.random.randint(len(sentences+sentences_test))
+    seed = (sentences+sentences_test)[seed_index]
 
-    for diversity in [0.1, 0.3, 0.5, 0.7, 0.9]:
+    for diversity in [0.2, 0.5, 0.8]:
         sentence = seed
         examples_file.write('----- Diversity:' + str(diversity) + '\n')
         examples_file.write('----- Generating with seed:\n"' + ' '.join(sentence) + '"\n')
         examples_file.write(' '.join(sentence))
 
-        for i in range(50):
-            # 1 - hot encoding of the input sequence
+        for i in range(50): #1 - hot encoding of the input sequence
             x_pred = np.zeros((1, SEQUENCE_LEN, len(words)))
             for t, word in enumerate(sentence):
                 x_pred[0, t, word_indices[word]] = 1.
@@ -90,37 +107,43 @@ def on_epoch_end(epoch, logs):
     examples_file.flush()
     
 if __name__ == "__main__":
-    corpus = 'F:/Minhaz/GitHubRepo/News_Gen/Minhaz/Code/Bengali_Work/corpus.txt'
-    examples = 'F:/Minhaz/GitHubRepo/News_Gen/Minhaz/Code/Bengali_Work/examples.txt'
-    vocabulary = 'F:/Minhaz/GitHubRepo/News_Gen/Minhaz/Code/Bengali_Work/vocab.txt'
+    directory = 'F:/Minhaz/GitHubRepo/News_Gen/Minhaz/Code/Bengali_Work/'
+    corpus_dir = directory + 'corpus/'
+    examples = directory + 'examples.txt'
+    vocabulary = directory + 'vocab.txt'
     
-    with io.open(corpus, encoding='utf-8') as f:
-        text = f.read()
-    print('Corpus length in characters:', len(text))
+    w_t = RegexpTokenizer("[\u0980-\u09FF']+")
+    corpus = PlaintextCorpusReader(corpus_dir, r'.*\.txt', word_tokenizer=w_t)
     
-    text_in_words = [w for w in text.split(' ') if w.strip() != '' or w != '\n']
+    text_in_words = corpus.words()
     print('Corpus length in words:', len(text_in_words))
 
     if not os.path.isdir('F:/Minhaz/GitHubRepo/News_Gen/Minhaz/Code/Bengali_Work/checkpoints/'):
         os.makedirs('F:/Minhaz/GitHubRepo/News_Gen/Minhaz/Code/Bengali_Work/checkpoints/')
     
-    words = sorted(set(text_in_words))
+    words = sorted(set(text_in_words))  #frequency, number, name analysis needed
     print_vocabulary(vocabulary, words)
 
     word_indices = dict((c, i) for i, c in enumerate(words))
     indices_word = dict((i, c) for i, c in enumerate(words))
     
+    files = corpus.fileids()
     sentences = []
     next_words = []
-    for i in range(0, len(text_in_words) - SEQUENCE_LEN, STEP):
-        sentences.append(text_in_words[i: i + SEQUENCE_LEN])
-        next_words.append(text_in_words[i + SEQUENCE_LEN])
+    for f in files:
+        words_in_file = corpus.words(f)
+        for i in range(0, len(words_in_file) - SEQUENCE_LEN, STEP):
+            sentences.append(words_in_file[i: i + SEQUENCE_LEN])
+            next_words.append(words_in_file[i + SEQUENCE_LEN])
     print('Using sequences:', len(sentences))
+        
+    (sentences, next_words), (sentences_test, next_words_test) = shuffle_and_split_training_set(
+        sentences, next_words)
     
     model = get_model()
     model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
     
-    file_path = "f:/Minhaz/GitHubRepo/News_Gen/Minhaz/Code/Bengali_Work/checkpoints/LSTM_LYRICS-epoch{epoch:03d}-words%d-sequence%d" % (len(words), SEQUENCE_LEN)
+    file_path = "f:/Minhaz/GitHubRepo/News_Gen/Minhaz/Code/Bengali_Work/checkpoints/LSTM_Bengali_News{epoch:03d}-words%d-sequence%d" % (len(words), SEQUENCE_LEN)
 
     checkpoint = ModelCheckpoint(file_path)
     print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
